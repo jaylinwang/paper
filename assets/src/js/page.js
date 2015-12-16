56c0dc532b6a071c8cba79f6cddc4f2d16c302e6
guovz <pzhuwjl@gmail.com>
2015-12-16 23:05:31 +0800

    文章评论数

--- assets/src/js/page.js
+++ assets/src/js/page.js
@@ -1,6 +1,19 @@
 (function ($) {
 	hljs.initHighlightingOnLoad();
 
+	$.get(ghost.url.api('posts', { fields: "id" })).done(function (data) {
+		var postCount = data.posts.length;
+		$('[data-toggle="postcount"]').text(postCount);
+	}).fail(function (err) {
+		console.log(err);
+	});
+
+	$.get(ghost.url.api('tags', { fields: "id" })).done(function (data) {
+		var tagCount = data.tags.length;
+		$('[data-toggle="tagcount"]').text(tagCount);
+	}).fail(function (err) {
+		console.log(err);
+	});
 	$.get(ghost.url.api('users/1')).done(function (data) {
 		var user = data.users[0];
 		$('[data-toggle="userimage"]').attr('src', user.image);
