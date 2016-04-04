64833e78fa1a50614d517bd222dbcae3f6e4a975
jaylin <mail.guovz@gmail.com>
2016-04-04 17:57:44 +0800

    添加title返回首页

--- assets/src/js/control.js
+++ assets/src/js/control.js
@@ -3,23 +3,23 @@
         sidebarToggle = '[data-toggle="sidebar"]',
         menuToggle = '[data-toggle="menu"]';
 
-    var toTop = function(){
-        $('html, body').animate({ scrollTop: 0 }, 200);
+    var toTop = function () {
+        $('html, body').animate({scrollTop: 0}, 200);
     }
-    
-    var toggleSidebar = function(e){
+
+    var toggleSidebar = function (e) {
         $('body').toggleClass('fold');
     }
-    
-    var toggleMenu = function(event){
+
+    var toggleMenu = function (event) {
         var $target = $(event.target).closest('[data-toggle="menu"]'),
             $toggleTarget = $($target.data('target'));
-        
+
         $toggleTarget.toggleClass('open');
     }
-    
-     $(document).on('click.totopToggle', totopToggle,toTop);
-     $(document).on('click.sidebarToggle',sidebarToggle,toggleSidebar);
-     $(document).on('click.menuToggle',menuToggle,toggleMenu);
-     
-} (jQuery));
+
+    $(document).on('click.totopToggle', totopToggle, toTop);
+    $(document).on('click.sidebarToggle', sidebarToggle, toggleSidebar);
+    $(document).on('click.menuToggle', menuToggle, toggleMenu);
+
+}(jQuery));
