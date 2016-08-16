a16d3630f68c00db0667afe359f204cd35a0cefd
JaylinWang <jaylin0019@gmail.com>
2016-08-16 21:53:49 +0800

    完善readme

--- README.md
+++ README.md
@@ -3,6 +3,16 @@ Ghost主题
 
 onepice是一款适用于ghost的博客主题。
 
+使用方式
+===
+
+克隆本主题到ghost程序目录下content/themes
+
+配置方式
+===
+
+参照config.example.js格式完成配置文件，填写至code injection>blog footer
+
 反馈
 ===
 
---
+++ config.example.js
@@ -0,0 +1,28 @@
+var onepice_config = {
+    ammount: {
+        enable: false,
+        leancloud: {
+
+            appId: '',
+            appKey: ''
+        }
+    },
+    comment: {
+        enable: false,
+        duoshuo: {
+            short_name: ''
+        },
+        disqus: {
+            short_name: ''
+        }
+    },
+    share: {
+        enable: true,
+        /**
+         * 百度分享配置
+         */
+        bdshare: {
+            enable: true
+        }
+    }
+};
